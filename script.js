3f416e3f2996f262702a91c5b6f1d64bfc2867f3
PilotSky1533512 <piro42489@gmail.com>
2026-03-14 15:13:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// シンプルなログイン機能
-function checkLogin() {
-    const id = document.getElementById('admin-id').value;
-    const pass = document.getElementById('admin-pass').value;
-
-    if (id === "admin" && pass === "1234") {
-        alert("成功しました！");
-        window.location.href = "admin.html";
-    } else {
-        alert("失敗：入力されたID=" + id + " パス=" + pass);
-    }
-}
-
 // モード切り替え
 document.getElementById('mode-toggle').onclick = () => {
     document.body.classList.toggle('dark-mode');
